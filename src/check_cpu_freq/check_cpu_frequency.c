b8cbe63ca5ae3584024ca7c3a8bc77b42a91b166
danielhufnagle <danielchufnagle@gmail.com>
2026-05-06 14:50:24 -0500

    added cpu frequency checking

--- src/check_cpu_freq/check_cpu_frequency.c
+++ src/check_cpu_freq/check_cpu_frequency.c
@@ -2,15 +2,38 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/cpufreq.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
 
-static int __init cpu_freq_init(void) {
-  unsigned int frequency = cpufreq_get(0);
-  printk("Current frequency: %u kHz\n", frequency);
-  return 0;
+#define POLL_INTERVAL_MS 1000
+
+static struct task_struct *freq_thread;
+
+static int freq_thread_fn(void *data)
+{
+    while (!kthread_should_stop()) {
+        unsigned int frequency = cpufreq_get(0);
+        printk(KERN_INFO "Current frequency: %u kHz\n", frequency);
+        msleep_interruptible(POLL_INTERVAL_MS);
+    }
+    return 0;
 }
 
-static void __exit cpu_freq_exit(void) {
-  printk("CPU frequency check completed\n");
+static int __init cpu_freq_init(void)
+{
+    printk(KERN_INFO "cpu_freq: starting\n");
+    freq_thread = kthread_run(freq_thread_fn, NULL, "cpu_freq_poller");
+    if (IS_ERR(freq_thread)) {
+        printk(KERN_ERR "cpu_freq: failed to create thread\n");
+        return PTR_ERR(freq_thread);
+    }
+    return 0;
+}
+
+static void __exit cpu_freq_exit(void)
+{
+    kthread_stop(freq_thread);
+    printk(KERN_INFO "cpu_freq: stopped\n");
 }
 
 module_init(cpu_freq_init);
@@ -19,4 +42,4 @@ module_exit(cpu_freq_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("DANIEL HUFNAGLE");
 MODULE_DESCRIPTION("Checking CPU frequency");
-MODULE_VERSION("0.0");
+MODULE_VERSION("0.1");
